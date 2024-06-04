37d4420a97f45e8db56a699ef7d77613651e6a7f
hkchukk <42998246+hkchukk@users.noreply.github.com>
2024-06-04 19:23:18 +0800

    Final

--- src/main/java/com/javaproject/ArgonPassword.java
+++ src/main/java/com/javaproject/ArgonPassword.java
@@ -1,27 +1,33 @@
 package com.javaproject;
+
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
+import java.util.Scanner;
 
 public class ArgonPassword {
 
     public static void main(String[] args) {
-        // 创建 Argon2 实例，使用 Argon2id 模式
+        // 創建 Argon2 實例，使用 Argon2id 模式
         Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
         
-        // 将密码定义在 try 块外部
-        char[] password = "123456".toCharArray(); // 这里输入你要加密的密码
+        // 使用 Scanner 讀取用戶輸入
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your password: ");
+        String inputPassword = scanner.nextLine();
+        char[] password = inputPassword.toCharArray();
 
         try {
-            // 生成密码哈希
+            // 生成密碼哈希
             String hash = argon2.hash(2, 65536, 1, password);
-            System.out.println("Generated hash: " + hash);
+            System.out.println("Hash: " + hash);
 
-            // 验证密码哈希
+            // 驗證密碼哈希
             boolean success = argon2.verify(hash, password);
-            System.out.println("Password verification status: " + success);
+            System.out.println("Verify: " + success);
         } finally {
-            // 清理可能留在内存中的密码
+            // 清理可能留在內存中的密碼
             argon2.wipeArray(password);
+            scanner.close();
         }
     }
 }
--- src/main/java/com/javaproject/ConfigurationEncryptor.java
+++ src/main/java/com/javaproject/ConfigurationEncryptor.java
@@ -2,15 +2,24 @@ package com.javaproject;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.iv.RandomIvGenerator;
+import java.util.Scanner;
 
 public class ConfigurationEncryptor {
     public static void main(String[] args) {
+        // 創建加密器實例
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        encryptor.setPassword("yekgF4qM69fmDP0vuN4iy6a12u3sFFPW"); // 设置加密密钥
+        encryptor.setPassword("yekgF4qM69fmDP0vuN4iy6a12u3sFFPW"); // 設置加密密鑰
         encryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
         encryptor.setIvGenerator(new RandomIvGenerator());
-        String mySecret = "ntoujava2024"; // 需要加密的敏感信息
-        String myEncryptedSecret = encryptor.encrypt(mySecret);
-        System.out.println("Encrypted: " + myEncryptedSecret);
+
+        try ( // 使用 Scanner 讀取用戶輸入
+                Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Original: ");
+            String mySecret = scanner.nextLine(); // 讀取敏感信息
+            // 加密敏感信息
+            String myEncryptedSecret = encryptor.encrypt(mySecret);
+            System.out.println("After: " + myEncryptedSecret);
+            // 關閉 Scanner
+        }
     }
 }
